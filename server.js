bcef1481fd3e243023c949698a2ad0ec1f7b9b05
venerati <jamesomarshall@gmail.com>
2015-07-24 14:37:59 -0400

    fixed syntax errors. server now runs properly.

--- server.js
+++ server.js
@@ -1,2 +1,43 @@
 var mongo = require('mongodb').MongoClient,
 	client = require('socket.io').listen(8080).sockets;
+
+mongo.connect('mongodb://127.0.0.1/chatdb', function(err, db){
+	if(err) throw err;
+
+	client.on('connection', function(socket) {
+
+		var col = db.collection('messages'),
+			sendStatus= function(s) {
+				socket.emit('status', s);
+			};	
+		// emit all messages
+		col.find().limit(100).sort({id: 1}).toArray(function(err, res) {
+			if(err) throw(err);
+			socket.emit('output', res)
+		});
+
+		//wait for input
+		socket.on('input', function(data){
+			var name = data.name,
+				message = data.message;
+				whitespacePattern = /^\s*$/;
+				//looking for whitespace in name or message
+			if(whitespacePattern.test(name) || whitespacePattern.test(message)) {
+				sendStatus('Name and message is required.');
+
+			} else {
+				col.insert({name: name, message: message}, function() {
+					
+					//emit latest message to all clients
+					client.emit('output', [data]);
+
+					sendStatus({
+						message: "Message sent",
+						clear: true
+					});
+				});
+			}
+		});
+	});
+});
+
