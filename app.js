5597bbd7d91b32984acfc2c969ef70491d8e2c30
venerati <jamesomarshall@gmail.com>
2015-07-24 14:47:26 -0400

    added output listener that loops through given array and populates previous messasges in chat window.

--- app.js
+++ app.js
@@ -6,8 +6,23 @@
 		},
 
 		//Get required nodes
-		textarea = getNode('.chat textarea');
-		chatName = getNode('.chatName');
+		status = getNode('.chatStatus span'),
+		messages =getNode('.chatMessages')
+		textarea = getNode('.chat textarea'),
+		chatName = getNode('.chatName'),
+
+		statusDefault = status.textContent,
+
+		setStatus = function() {
+			status.textContent = s;
+
+			if (s !== statusDefault) {
+				var delay = setTimeout(function() {
+					setStatus(statusDefault);
+					clearInterval(delay);
+				}; 3000);
+			};
+		};
 
 		try {
 			var socket = io.connect('http://127.0.0.1:8080');
@@ -15,6 +30,32 @@
 			//set status to warn user
 		}
 		if(socket !== undefined) {
+
+			//listen for output
+			socket.on('output',function(data) {
+				if(data.length) {
+					//loop through results
+					for (var x = 0; x < data.length; x = x + 1) {
+						var message = document.createElement('div');
+						message.setAttribute('class', 'chatMessage');
+						message.textContent = data[x].name + ': ' + data[x].message;
+
+						//append
+						messages.appendChild(message);
+						messages.insertBefore (message, messages.firstChild)
+
+					};
+				}
+			});
+
+			//listen for a status
+			socket.on('status', function(data){
+				setStatus((typeof data === 'object') ? data.message : data);
+
+				if (data.clear === true) {
+					textarea.value = '';
+				};
+			});
 			
 			//listen to text area
 			textarea.addEventListener('keydown', function(event) {
